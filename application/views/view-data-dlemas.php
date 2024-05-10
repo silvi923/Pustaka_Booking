ea4840c4de5adccf921930ae27de6482edada1b4
silvi923 <silviayuni475@gmail.com>
2024-05-10 14:20:09 +0700

    Tugas 6

---
+++ application/views/view-data-dlemas.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <title>Tampil Data Dlemas</title>
+</head>
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">Tampil Data Siswa</th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                    <td><?= $nama; ?></td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                    <td><?= $nis; ?></td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                    <td><?= $kelas; ?></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                    <td><?= $tanggal_lahir; ?></td>
+            </tr>
+            <tr>
+                <td>Tempat Lahir</td>
+                <td>:</td>
+                    <td><?= $tempat_lahir; ?></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                    <td><?= $alamat; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                    <td><?= $jenis_kelamin; ?></td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                    <td><?= $agama; ?></td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center"><a href="<?= base_url('dlemas');?>">Kembali</a></td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
